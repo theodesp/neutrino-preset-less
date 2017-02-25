b00ca3a8f4ed00e938361d8518d2b454696dad9d
Theofanis Despoudis <thdespou@hotmail.com>
2017-02-25 16:07:34 +0000

    Initial support using less-loader

---
+++ README.md
@@ -0,0 +1,8 @@
+# Neutrino Less Preset
+
+`neutrino-preset-less` is a Neutrino preset that that adds support for less using the (less-loader)[https://github.com/webpack-contrib/less-loader].
+
+## Documentation
+
+See the [Neutrino docs](https://neutrino.js.org/)
+for information about Neutrino
---
+++ src/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const CWD = process.cwd();
+const SRC = path.join(CWD, 'src');
+const MODULES = path.join(__dirname, '../node_modules');
+const CSS_LOADER = require.resolve('css-loader');
+const STYLE_LOADER = require.resolve('style-loader');
+const LESS_LOADER = require.resolve('less-loader');
+const IF_NOT_DEV = process.env.NODE_ENV !== 'development';
+
+module.exports = neutrino => {
+    const { config } = neutrino;
+    config.module
+        .rule('less')
+        .test(/\.less$/)
+        .pre()
+        .include(SRC)
+        .loader('style', STYLE_LOADER)
+        .loader('css', CSS_LOADER)
+        .loader('less', STYLE_LOADER); 
+    
+    config.resolve.modules.add(MODULES);
+    config.resolveLoader.modules.add(MODULES);
+}
